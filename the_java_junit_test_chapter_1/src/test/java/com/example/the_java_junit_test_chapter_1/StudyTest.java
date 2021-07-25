980aed23238b5633f2cfeebb9e105165d35c3787
offMomySon <ohji100629@naver.com>
2021-07-25 21:56:25 +0900

    JUnit test name display ch1_2

--- the_java_junit_test_chapter_1/src/test/java/com/example/the_java_junit_test_chapter_1/StudyTest.java
+++ the_java_junit_test_chapter_1/src/test/java/com/example/the_java_junit_test_chapter_1/StudyTest.java
@@ -4,17 +4,18 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
     @Test
-    void create(){
+    @DisplayName("스터디 만들기")
+    void create_new_study(){
         Study study = new Study();
         assertNotNull(study);
         System.out.println("create");
     }
 
     @Test
-    @Disabled
-    void create1(){
+    void create_new_study_again(){
         Study study = new Study();
         assertNotNull(study);
         System.out.println("create1");
